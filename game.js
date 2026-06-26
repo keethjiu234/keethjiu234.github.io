5cb9b2b5550c3bb8f91cc33a236ae3417609d1d4
keethjiu234 <samcrossley101@gmail.com>
2026-06-26 14:16:13 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,147 +1,91 @@
+import * as THREE from 'three';
+
 const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
 
-const entities = [];
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 2, 5);
 
-const camera = {
-    x: 0,
-    y: 0,
-    width: canvas.width,
-    height: canvas.height
-};
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 10, 5);
+scene.add(light);
 
-const player = new Player(
-    new Vector2(100, 100),
-    new Vector2(50, 50)
-);
-
-let lastTime = 0;
-
-//classes
-
-class Vector2 {
-    constructor(x = 0, y = 0) {
+class Vector3 {
+    constructor(x = 0, y = 0, z = 0) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
-    add(v) { 
-        this.x += v.x; 
-        this.y += v.y; 
-        return this; 
-    }
-    scale(s) {
-        this.x *= s; 
-        this.y *= s; 
-        return this; 
-    }
-    clone() { 
-        return new Vector2(this.x, this.y); 
-    }
-    set(x, y) { 
-        this.x = x; 
-        this.y = y; 
-        return this; 
-    }
+    add(v) { this.x += v.x; this.y += v.y; this.z += v.z; return this; }
+    scale(s) { this.x *= s; this.y *= s; this.z *= s; return this; }
+    clone() { return new Vector3(this.x, this.y, this.z); }
+    set(x, y, z) { this.x = x; this.y = y; this.z = z; return this; }
 }
 
 class Entity {
-    constructor(position, size) {
+    constructor(position, size, color = 0xff00ff) {
         this.position = position;
         this.size = size;
-        this.velocity = new Vector2(0, 0);
+        this.velocity = new Vector3(0, 0, 0);
+
+        const geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
+        const material = new THREE.MeshStandardMaterial({ color });
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.mesh.position.set(position.x, position.y, position.z);
+        scene.add(this.mesh);
     }
 
     update(deltaTime) {
-        this.position.x += this.velocity.x * deltaTime;
-        this.position.y += this.velocity.y * deltaTime;
-    }
-
-    draw(ctx) {
-        ctx.fillStyle = "magenta";
-        ctx.fillRect(
-            this.position.x,
-            this.position.y,
-            this.size.x,
-            this.size.y
-        );
+        this.position.add(this.velocity.clone().scale(deltaTime));
+        this.mesh.position.set(this.position.x, this.position.y, this.position.z);
     }
 }
 
 class Player extends Entity {
     constructor(position, size) {
-        super(position, size);
-        this.speed = 250; 
+        super(position, size, 0x1e90ff);
+        this.speed = 5;
     }
 
     update(deltaTime) {
-        this.velocity.set(0, 0);
+        this.velocity.set(0, 0, 0);
 
-        if (keys["w"]) this.velocity.y = -this.speed;
-        if (keys["s"]) this.velocity.y = this.speed;
+        if (keys["w"]) this.velocity.z = -this.speed;
+        if (keys["s"]) this.velocity.z = this.speed;
         if (keys["a"]) this.velocity.x = -this.speed;
         if (keys["d"]) this.velocity.x = this.speed;
 
-        if (this.velocity.x !== 0 && this.velocity.y !== 0) {
-            const inv = Math.SQRT1_2;
-            this.velocity.x *= inv;
-            this.velocity.y *= inv;
-        }
-
         super.update(deltaTime);
     }
-
-    draw(ctx) {
-        ctx.fillStyle = "dodgerblue";
-        ctx.fillRect(
-            this.position.x,
-            this.position.y,
-            this.size.x,
-            this.size.y
-        );
-    }
 }
 
-//actual game funcs
+const keys = {};
+window.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
+window.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
 
-function isOnScreen(entity) {
-    return !(
-        entity.position.x + entity.size.x < camera.x ||
-        entity.position.x > camera.x + camera.width ||
-        entity.position.y + entity.size.y < camera.y ||
-        entity.position.y > camera.y + camera.height
-    );
-}
+const entities = [];
+const player = new Player(new Vector3(0, 0.5, 0), new Vector3(1, 1, 1));
+entities.push(player);
 
+const groundGeometry = new THREE.PlaneGeometry(20, 20);
+const groundMaterial = new THREE.MeshStandardMaterial(0xff00ff);
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2;
+scene.add(ground);
 
+let lastTime = 0;
 function gameLoop(timestamp) {
     const deltaTime = (timestamp - lastTime) / 1000;
     lastTime = timestamp;
 
-    update(deltaTime);
-    draw();
+    for (const e of entities) e.update(deltaTime);
+
+    camera.lookAt(player.mesh.position);
+    renderer.render(scene, camera);
 
     requestAnimationFrame(gameLoop);
 }
-
-function update(deltaTime) {
-    for (const e of entities) {
-        e.update(deltaTime);
-    }
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (const e of entities) {
-        if (isOnScreen(e)) {
-            e.draw(ctx);
-        }
-    }
-}
-
-
-//onstart
-
-entities.push(player);
-
 requestAnimationFrame(gameLoop);
